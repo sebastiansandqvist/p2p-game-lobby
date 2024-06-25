2a8aaab5f6978159ef8c0af0a2df632e5fc07cdb
Sebastian Sandqvist <seb@sparque.me>
2024-06-24 19:34:47 -0700

    shorter game id

--- examples/tictactoe/src/p2p.ts
+++ examples/tictactoe/src/p2p.ts
@@ -101,5 +101,5 @@ function gameId() {
 }
 
 function randomId() {
-  return (Math.random() * 100_000).toString().replace('.', '');
+  return (Math.random() * 100_000).toString().replace('.', '').slice(0, 6);
 }
